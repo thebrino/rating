9755e424c3214c48279dbe7a8408cad5dad60062
thebrino <bm13141455@gmail.com>
2023-02-09 17:17:01 -0300

    added 'rate again' button

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var rate = [1, 2, 3, 4, 5];
+let rate = [1, 2, 3, 4, 5];
 
 const thanks = document.getElementById("thanks");
 const submit = document.getElementById("submit");
+const rateAgain = document.getElementById("rateagain");
 const buttons = document.getElementById("buttons");
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 const button4 = document.getElementById("button4");
 const button5 = document.getElementById("button5");
-var desc = document.getElementById("desc");
+let desc = document.getElementById("desc");
 
 submit.addEventListener("click", () => {
   tyScreen();
@@ -16,6 +17,14 @@ submit.addEventListener("click", () => {
 
 function tyScreen() {
   thanks.classList.remove("none");
+}
+
+rateAgain.addEventListener("click", () => {
+  homeScreen();
+});
+
+function homeScreen() {
+  thanks.classList.add("none");
 }
 
 button1.addEventListener("click", () => {
